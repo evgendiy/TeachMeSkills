20ad345a3dc2c2df70f3dc4bece7b73de777c9e4
Evgeniy Pokatashkin <evgeniypokat@gmail.com>
2024-01-07 23:27:29 +0300

    Added extratask3, try to make pull request

---
+++ src/homework4/ExtraTask3.java
@@ -0,0 +1,9 @@
+package homework4;
+/**
+ * Создайте двумерный массив и выведите на консоль сумму всех его элементов
+ */
+public class ExtraTask3 {
+    public static void main(String[] args) {
+
+    }
+}
